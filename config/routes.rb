1e88b328869847aac43f472581b706f92889e9c2
Guillaume Hercot <guillaume.hercot@gmail.com>
2017-12-04 15:46:11 +0100

    Created controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :trip, only: [:show, :new, :create, :edit, :update, :destroy] do
+    resources :visit, only: [:create, :destroy]
+  end
+  resources :activity, only: [:index, :show, :create, :update, :destroy]
 end
