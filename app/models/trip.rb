11ca06fb75389c26f44cc87892965e8c528496c1
Alexia Le Tarnec <alexialetarnec@gmail.com>
2017-12-05 16:06:04 +0100

    finished new form back

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,7 @@
 class Trip < ApplicationRecord
   #Associations
   belongs_to :host, class_name: 'User'
-  belongs_to :tourist, class_name: 'User'
+  # belongs_to :tourist, class_name: 'User'
   has_many :visits, dependent: :destroy
   has_many :activities, through: :visits
 
@@ -9,7 +9,7 @@ class Trip < ApplicationRecord
   validates :title, presence: true
   validates :date, presence: true
   validates :host_id, presence: true
-  validates :tourist_id, presence: true
+  # validates :tourist_id, presence: true
   validates :budget, inclusion: { in: [1, 2, 3, 4, 5] }, allow_blank: true
   validates :knows_the_city, inclusion: { in: [1, 2, 3, 4, 5] }, allow_blank: true
   # validates :tourist_id, uniqueness: { scope: :host_id } # Prevent creating a trip for self
