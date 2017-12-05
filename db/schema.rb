9405ad5b85ce2e8efe920f30df74bfe3d04508ac
Julie Simon <juliesimon.js@gmail.com>
2017-12-05 12:14:04 +0100

    seeding 10 museums

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205093017) do
+ActiveRecord::Schema.define(version: 20171205104615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 20171205093017) do
     t.string "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
+    t.string "title"
   end
 
   create_table "trips", force: :cascade do |t|
