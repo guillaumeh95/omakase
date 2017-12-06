c04db740c54bf0c3a51df88b9c9033cdb83c37b9
Guillaume Hercot <guillaume.hercot@gmail.com>
2017-12-06 15:36:18 +0100

    Dashboard done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205132158) do
+ActiveRecord::Schema.define(version: 20171206110659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20171205132158) do
     t.integer "budget"
     t.integer "knows_the_city"
     t.string "status"
+    t.boolean "sent", default: false
   end
 
   create_table "users", force: :cascade do |t|
