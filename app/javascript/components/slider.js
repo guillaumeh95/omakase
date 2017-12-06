34a0003ec59dcd8e25085c03c219f7779ed96af9
Alexia Le Tarnec <alexialetarnec@gmail.com>
2017-12-06 18:09:38 +0100

    almost completed form

--- app/javascript/components/slider.js
+++ app/javascript/components/slider.js
@@ -9,15 +9,13 @@ function initSliders() {
     steps: 4,
     value: 3,
     orientation: 'horizontal',
-    ticks: [1, 2, 3, 4, 5]
   });
 $('.sliderknowledge').slider({
     min: 1,
     max: 5,
     steps: 4,
     value: 3,
-    orientation: 'horizontal',
-    ticks: [1, 2, 3, 4, 5]
+    orientation: 'horizontal'
     });
 
 };
