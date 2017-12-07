9ba2a1163db7fd9c499d185de8e49394b26846c6
Guillaume Hercot <guillaume.hercot@gmail.com>
2017-12-07 17:01:33 +0100

    alert disappearing after few seconds

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,44 +10,13 @@
 import 'bootstrap';
 import "../plugins/flatpickr";
 import "../components/trip_form";
-
+import "../components/cards_dashboard";
 import {initSliders} from "../components/slider";
 initSliders();
+import { setAlertsTimeouts } from '../components/alert';
 
-
-const cards = document.querySelectorAll(".card.full-card");
-
-if (cards) {
-  cards.forEach((card) => {
-    card.addEventListener("mouseenter", (event) => {
-      const target = event.currentTarget.querySelector(".trip-details");
-      target.classList.remove('hidden');
-      target.classList.add('fade-card');
-      const title = event.currentTarget.querySelector(".trip-title");
-      title.classList.remove('trip-title');
-      title.classList.add('trip-title-large', 'fade-card');
-
-    });
-    card.addEventListener("mouseleave", (event) => {
-      const target = event.currentTarget.querySelector(".trip-details");
-      target.classList.add('hidden');
-      target.classList.remove('fade-card');
-      const title = event.currentTarget.querySelector(".trip-title-large");
-      title.classList.remove('trip-title-large');
-      title.classList.add('trip-title', 'fade-card');
-    });
-  });
-}
-
-
-const activities = document.querySelectorAll(".card-activity-unit")
-
-if (activities) {
-  activities.forEach((activity) => {
-    activity.addEventListener("click", (event) => {
-      const activityId = event.currentTarget.dataset.activityId
-      console.log(activityId)
-    })
-  })
+// Alerts disappearing automatically
+if (document.querySelector(".alert")) {
+  setAlertsTimeouts();
 }
 
