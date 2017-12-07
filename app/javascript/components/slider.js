0571174b3afff99c84642db80251ffd544cb085c
Guillaume Hercot <guillaume.hercot@gmail.com>
2017-12-07 18:38:49 +0100

    Finished sliders - labels flexbos missing

--- app/javascript/components/slider.js
+++ app/javascript/components/slider.js
@@ -9,6 +9,8 @@ function initSliders() {
     steps: 4,
     value: 3,
     orientation: 'horizontal',
+    ticks: [1, 2, 3, 4, 5],
+    ticks_labels: ['Poor', 'Average', 'Not Sure', 'Rich', 'Very Rich']
   });
 $('.sliderknowledge').slider({
     min: 1,
@@ -17,7 +19,7 @@ $('.sliderknowledge').slider({
     value: 3,
     orientation: 'horizontal',
     ticks: [1, 2, 3, 4, 5],
-    ticks_labels: ['Very Poor', 'Poor', 'Irrelevant', 'Rich', 'Very Rich']
+    ticks_labels: ['Not At All', 'Little', 'Not Sure', 'Good', 'Well']
     });
 
 };
