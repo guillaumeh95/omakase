e460653fd83b29516b2d25f292f1dc5be1698f12
Mathilde Ganancia <mathilde.ganancia@gmail.com>
2017-12-07 14:40:51 +0100

    beginning map

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,15 @@ class UsersController < ApplicationController
     @trips_tourist = Trip.where(tourist_id: current_user.id)
     @trips_tourist_incoming = @trips_tourist.select {|trip| Time.now <= trip.date}
     @trips_tourist_past = @trips_tourist.select {|trip| Time.now > trip.date}
+
+    @activities = Activity.where.not(latitude: nil, longitude: nil)
+
+    @markers = @activities.map do |activity|
+      {
+        lat: activity.latitude,
+        lng: activity.longitude#,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+      }
+    end
   end
 end
