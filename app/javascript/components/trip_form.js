88829273b204b929f8a743cc907bc01329bd7f5c
Alexia Le Tarnec <alexialetarnec@gmail.com>
2017-12-08 12:10:16 +0100

    completed homepage and form new profile

--- app/javascript/components/trip_form.js
+++ app/javascript/components/trip_form.js
@@ -1,36 +1,3 @@
-// sélectionner les checkboxes
-const descriptionBoxes = document.querySelectorAll('.select-description');
-// parcourir les checkboxes
-descriptionBoxes.forEach((descriptionBox) => {
-  // écouter le click sur chacune d'entre elles
-  descriptionBox.addEventListener('click', (event) => {
-    const activeBoxes = document.querySelectorAll('.select-description.active');
-    if (event.currentTarget.classList.contains('active')) {
-      event.currentTarget.classList.remove('active');
-      event.currentTarget.querySelector('input[type=hidden]').value = false;
-    } else {
-      // si déjà 3 éléments sélectionnés
-      if (activeBoxes.length === 3) {
-        alert('you can select only three profiles');
-      } else {
-        event.currentTarget.classList.add('active');
-        // ajouter la value true au input hidden correspondant
-        event.currentTarget.querySelector('input[type=hidden]').value = true;
-      }
-    }
-    const submit = document.getElementById('submit-trip');
-    // compter le nombre de checkboxes cochées
-    // si yen a 3
-    if (activeBoxes.length === 2) {
-      // on enable le submit
-      submit.disabled = false;
-    } else {
-      // on disable
-      submit.disabled = true;
-    }
-  });
-});
-
 const statusBoxes = document.querySelectorAll('.select-status');
 // parcourir les checkboxes
 statusBoxes.forEach((statusBox) => {
@@ -43,7 +10,7 @@ statusBoxes.forEach((statusBox) => {
     } else {
       // si déjà 3 éléments sélectionnés
       if (activeBoxes.length === 1) {
-        alert('you can select only three profiles');
+        alert('Please select only one status');
       } else {
         event.currentTarget.classList.add('active');
         // ajouter la value true au input hidden correspondant
@@ -51,6 +18,37 @@ statusBoxes.forEach((statusBox) => {
       }
     }
   });
-
-
 });
+  const activeBoxes = document.querySelectorAll('.select-status.active');
+  const descriptionBoxes = document.querySelectorAll('.select-description');
+  // parcourir les checkboxes
+  descriptionBoxes.forEach((descriptionBox) => {
+    // écouter le click sur chacune d'entre elles
+    descriptionBox.addEventListener('click', (event) => {
+      const activeBoxes = document.querySelectorAll('.select-description.active');
+      if (event.currentTarget.classList.contains('active')) {
+        event.currentTarget.classList.remove('active');
+        event.currentTarget.querySelector('input[type=hidden]').value = false;
+      } else {
+        // si déjà 3 éléments sélectionnés
+        if (activeBoxes.length === 3) {
+          alert('you can select only three profiles');
+        } else {
+          event.currentTarget.classList.add('active');
+          // ajouter la value true au input hidden correspondant
+          event.currentTarget.querySelector('input[type=hidden]').value = true;
+        }
+      }
+      const submit = document.getElementById('submit-trip');
+      // compter le nombre de checkboxes cochées
+      // si yen a 3
+      if (activeBoxes.length === 2 ) {
+        // on enable le submit
+        submit.disabled = false;
+      } else {
+        // on disable
+        submit.disabled = true;
+      }
+    });
+  });
+
