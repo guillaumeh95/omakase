c44ec8f86a95f431e5fdb722ddc27340e116695a
Guillaume Hercot <guillaume.hercot@gmail.com>
2017-12-11 18:00:13 +0100

    added routes

---
+++ app/mailers/trip_mailer.rb
@@ -0,0 +1,9 @@
+class TripMailer < ApplicationMailer
+  def send_trip(trip)
+    @trip = trip
+    mail(
+          to:       @trip.tourist_email,
+          subject:  "#{@trip.host.first_name.capitalize} sent you a new trip!"
+        )
+  end
+end
