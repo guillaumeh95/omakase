6e37d320a4894de25f5ae644a296d92bbaac0410
Mathilde Ganancia <mathilde.ganancia@gmail.com>
2017-12-14 17:41:55 +0100

    newshow

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,156 +1,194 @@
 function initMap() {
   const mapElement = document.getElementById('map');
   const markers = JSON.parse(mapElement.dataset.markers);
-
-  var marker0 = {lat: markers[0].lat, lng: markers[0].lng};
-  var marker1 = {lat: markers[1].lat, lng: markers[1].lng};
-  var marker2 = {lat: markers[2].lat, lng: markers[2].lng};
-  var markerArray = [marker0, marker1, marker2];
-
+  var marker0 = markers[0]
+  var marker1 = markers[1]
+  var marker2 = markers[2]
+  var markerArray = markers;
   var styledMapType = new google.maps.StyledMapType(
-          [
-  {
-      "featureType": "all",
-      "elementType": "labels",
-      "stylers": [
-          {
-              "visibility": "off"
-          }
-      ]
-  },
-  {
-      "featureType": "all",
-      "elementType": "labels.text",
-      "stylers": [
-          {
-              "visibility": "on"
-          }
-      ]
-  },
-  {
-      "featureType": "all",
-      "elementType": "labels.text.fill",
-      "stylers": [
-          {
-              "color": "#000000"
-          }
-      ]
-  },
-  {
-      "featureType": "all",
-      "elementType": "labels.text.stroke",
-      "stylers": [
-          {
-              "color": "#ffffff"
-          }
-      ]
-  },
-  {
-      "featureType": "all",
-      "elementType": "labels.icon",
-      "stylers": [
-          {
-              "visibility": "on"
-          }
-      ]
-  },
-  {
-      "featureType": "administrative",
-      "elementType": "all",
-      "stylers": [
-          {
-              "visibility": "off"
-          }
-      ]
-  },
-  {
-      "featureType": "administrative.neighborhood",
-      "elementType": "geometry.fill",
-      "stylers": [
-          {
-              "color": "#eaf1ff"
-          }
-      ]
-  },
-  {
-      "featureType": "landscape",
-      "elementType": "all",
-      "stylers": [
-          {
-              "color": "#ffffff"
-          }
-      ]
-  },
-  {
-      "featureType": "poi",
-      "elementType": "all",
-      "stylers": [
-          {
-              "visibility": "off"
-          }
-      ]
-  },
-  {
-      "featureType": "road",
-      "elementType": "geometry.fill",
-      "stylers": [
-          {
-              "color": "#000000"
-          },
-          {
-              "weight": 1
-          }
-      ]
-  },
-  {
-      "featureType": "road",
-      "elementType": "geometry.stroke",
-      "stylers": [
-          {
-              "color": "#000000"
-          },
-          {
-              "weight": 0.8
-          }
-      ]
-  },
-  {
-      "featureType": "road",
-      "elementType": "labels",
-      "stylers": [
-          {
-              "visibility": "on"
-          }
-      ]
-  },
-  {
-      "featureType": "transit",
-      "elementType": "all",
-      "stylers": [
-          {
-              "visibility": "off"
-          }
-      ]
-  },
-  {
-      "featureType": "water",
-      "elementType": "all",
-      "stylers": [
-          {
-              "visibility": "off"
-          }
-      ]
-  }
-],
 
-  {name: 'Styled Map'});
+    [
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e9e9e9"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dedede"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#333333"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    }
+], { name: 'Styled Map' });
 
 
   var map = new google.maps.Map(document.getElementById('map'), {
     center: marker0,
     mapTypeControlOptions: {
-    mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
-              'styled_map']
+      mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain', 'styled_map']
     }
   });
 
@@ -159,47 +197,27 @@ function initMap() {
   map.setMapTypeId('styled_map');
 
 
-  var place1 = new google.maps.Marker({
-    position: marker0,
-    map: map,
-    title: 'Marker0'
-  });
-
-  var place2 = new google.maps.Marker({
-    position: marker1,
-    map: map,
-    title: 'Marker1'
-  });
-
-  var place3 = new google.maps.Marker({
-    position: marker2,
-    map: map,
-    title: 'Marker2'
-  });
-
-
-  var image1 = 'http://res.cloudinary.com/djlkudqpv/image/upload/c_scale,w_45/v1513163786/1_ljmdmd.png';
+  var image1 = 'http://res.cloudinary.com/djlkudqpv/image/upload/c_scale,w_35/v1513163786/1_ljmdmd.png';
   var imageMarker1 = new google.maps.Marker({
     position: marker0,
     map: map,
     icon: image1
   });
 
-  var image2 = 'http://res.cloudinary.com/djlkudqpv/image/upload/c_scale,w_45/v1513163787/2_c5kne3.png';
+  var image2 = 'http://res.cloudinary.com/djlkudqpv/image/upload/c_scale,w_35/v1513163787/2_c5kne3.png';
   var imageMarker2 = new google.maps.Marker({
     position: marker1,
     map: map,
     icon: image2
   });
 
-  var image3 = 'http://res.cloudinary.com/djlkudqpv/image/upload/c_scale,w_45/v1513163787/3_i4d0wn.png';
+  var image3 = 'http://res.cloudinary.com/djlkudqpv/image/upload/c_scale,w_35/v1513163787/3_i4d0wn.png';
   var imageMarker3 = new google.maps.Marker({
     position: marker2,
     map: map,
     icon: image3
-  });
+      });
 
-  var placeArray = [place1, place2, place3];
 
   // Instantiate a directions service.
   var directionsService = new google.maps.DirectionsService;
@@ -209,19 +227,17 @@ function initMap() {
   const selectedMode = document.getElementById('mode').value;
 
   // Create a renderer for directions and bind it to the map.
-  var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
-  var directionsDisplay2 = new google.maps.DirectionsRenderer({map: map});
+  var directionsDisplay = new google.maps.DirectionsRenderer({map: map, polylineOptions:{strokeColor:"#4a4a4a",strokeWeight:4}, suppressMarkers: true});
+  var directionsDisplay2 = new google.maps.DirectionsRenderer({ map: map, polylineOptions:{strokeColor:"#4a4a4a",strokeWeight:4}, suppressMarkers:true})
 
   // Instantiate an info window to hold step text.
   var stepDisplay = new google.maps.InfoWindow;
 
   // Display the route between the initial start and end selections.
-  calculateAndDisplayRoute(
-      directionsDisplay, directionsDisplay2, directionsService, directionsService2, markerArray, placeArray, stepDisplay, selectedMode, map);
+  calculateAndDisplayRoute(directionsDisplay, directionsDisplay2, directionsService, directionsService2, markers, stepDisplay, selectedMode, map);
   // Listen to change events from the start and end lists.
   var onChangeHandler = function() {
-    calculateAndDisplayRoute(
-       directionsDisplay, directionsDisplay2, directionsService, directionsService2, markerArray, placeArray, stepDisplay, selectedMode, map);
+    calculateAndDisplayRoute(directionsDisplay, directionsDisplay2, directionsService, directionsService2, markers, stepDisplay, selectedMode, map);
   };
 
   document.getElementById('mode').addEventListener('change', onChangeHandler);
@@ -229,60 +245,32 @@ function initMap() {
   //Associate the styled map with the MapTypeId and set it to display.
   map.mapTypes.set('styled_map', styledMapType);
   map.setMapTypeId('styled_map');
+
+  // fit bounds to map fitBounds fitLatLng(markers)
+  //fitBoundsToVisibleMarkers(markers);
+
 }
 
-      function calculateAndDisplayRoute(directionsDisplay, directionsDisplay2, directionsService, directionsService2,
-          markerArray, placeArray, stepDisplay, selectedMode, map) {
-        // First, remove any existing markers from the map.
-        for (var i = 0; i < placeArray.length; i++) {
-          placeArray[i].setMap(null);
-        }
+// function fitBoundsToVisibleMarkers(markers) {
 
-        // Retrieve the start and end locations and create a DirectionsRequest using
-        // WALKING directions.
-        directionsService.route({
-          origin: markerArray[0],
-          destination: markerArray[1],
-          travelMode: google.maps.TravelMode[selectedMode]
-        }, function(response, status) {
-          if (status === 'OK') {
-            // var inhtml = document.querySelectorAll('gm-iw')
-            // console.log(inhtml)
-            //     '<b>' + response.routes[0].warnings + '</b>';
-            directionsDisplay.setDirections(response);
-            // showSteps(response, placeArray, stepDisplay, map);
-          } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-        });
+//     var bounds = new google.maps.LatLngBounds();
 
-        directionsService2.route({
-          origin: markerArray[1],
-          destination: markerArray[2],
-          travelMode: google.maps.TravelMode[selectedMode]
-        }, function(response2, status) {
-          if (status === 'OK') {
-            // document.getElementById('warnings-panel').innerHTML =
-            //     '<b>' + response.routes[0].warnings + '</b>';
-            directionsDisplay2.setDirections(response2);
-            // showSteps(response, placeArray, stepDisplay, map);
-          } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-        });
-          // Route the directions and pass the response to a function to create
-          // markers for each step.
-          attachInstructionText(
-              stepDisplay, placeArray, map);
-      }
+//     for (var i=0; i<markers.length; i++) {
+//       console.log(markers[i]);
+//         if(markers[i].getVisible()) {
+//             bounds.extend( markers[i].getPosition() );
+//         }
+//    }
 
-function calculateAndDisplayRoute(directionsDisplay, directionsDisplay2, directionsService, directionsService2,
-    markerArray, placeArray, stepDisplay, selectedMode, map) {
+//     map.fitBounds(bounds);
+
+//}
+
+function calculateAndDisplayRoute(directionsDisplay, directionsDisplay2, directionsService, directionsService2, markerArray, stepDisplay, selectedMode, map) {
   // First, remove any existing markers from the map.
-  for (var i = 0; i < placeArray.length; i++) {
-    placeArray[i].setMap(null);
-  }
-
+  // for (var i = 0; i < placeArray.length; i++) {
+  //   placeArray[i].setMap(null);
+  // }
 
   // Retrieve the start and end locations and create a DirectionsRequest using
   // WALKING directions.
@@ -292,9 +280,6 @@ function calculateAndDisplayRoute(directionsDisplay, directionsDisplay2, directi
     travelMode: google.maps.TravelMode[selectedMode]
   }, function(response, status) {
     if (status === 'OK') {
-      // var inhtml = document.querySelectorAll('gm-iw')
-      // console.log(inhtml)
-      //     '<b>' + response.routes[0].warnings + '</b>';
       directionsDisplay.setDirections(response);
       // showSteps(response, placeArray, stepDisplay, map);
     } else {
@@ -316,82 +301,7 @@ function calculateAndDisplayRoute(directionsDisplay, directionsDisplay2, directi
       window.alert('Directions request failed due to ' + status);
     }
   });
-    // Route the directions and pass the response to a function to create
-    // markers for each step.
-    attachInstructionText(
-        stepDisplay, placeArray, map);
-}
 
-function calculateAndDisplayRoute(directionsDisplay, directionsDisplay2, directionsService, directionsService2,
-    markerArray, placeArray, stepDisplay, selectedMode, map) {
-  // First, remove any existing markers from the map.
-  for (var i = 0; i < placeArray.length; i++) {
-    placeArray[i].setMap(null);
-  }
-
-  // Retrieve the start and end locations and create a DirectionsRequest using
-  // WALKING directions.
-  directionsService.route({
-    origin: markerArray[0],
-    destination: markerArray[1],
-    travelMode: google.maps.TravelMode[selectedMode]
-  }, function(response, status) {
-    if (status === 'OK') {
-      // var inhtml = document.querySelectorAll('gm-iw')
-      // console.log(inhtml)
-      //     '<b>' + response.routes[0].warnings + '</b>';
-      directionsDisplay.setDirections(response);
-      // showSteps(response, placeArray, stepDisplay, map);
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  });
-
-  directionsService2.route({
-    origin: markerArray[1],
-    destination: markerArray[2],
-    travelMode: google.maps.TravelMode[selectedMode]
-  }, function(response2, status) {
-    if (status === 'OK') {
-      // document.getElementById('warnings-panel').innerHTML =
-      //     '<b>' + response.routes[0].warnings + '</b>';
-      directionsDisplay2.setDirections(response2);
-      // showSteps(response, placeArray, stepDisplay, map);
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  });
-    // Route the directions and pass the response to a function to create
-    // markers for each step.
-    attachInstructionText(
-        stepDisplay, placeArray, map);
-}
-
-function showSteps(directionResult, placeArray, stepDisplay, map) {
-  // For each step, place a marker, and add the text to the marker's infowindow.
-  // Also attach the marker to an array so we can keep track of it and remove it
-  // when calculating new routes.
-  console.log("showsteps")
-  var myRoute = directionResult.routes[0].legs[0];
-  for (var i = 0; i < myRoute.steps.length; i++) {
-    var marker = placeArray[i] = placeArray[i] || new google.maps.Marker;
-    //to show marker instructions
-    //marker.setMap(map);
-    //marker.setPosition(myRoute.steps[i].start_location);
-  }
-}
-
-function attachInstructionText(stepDisplay, placeArray, text, map) {
-  console.log("instructionstxt")
-  for (var i = 0; i < placeArray.length; i++) {
-    console.log(placeArray[i]);
-    google.maps.event.addListener(placeArray[i], 'click', function() {
-      // Open an info window when the marker is clicked on, containing the text
-      // of the step.
-      stepDisplay.setContent("HELLO");
-      stepDisplay.open(map, marker);
-    });
-  }
 }
 
 export { initMap };
