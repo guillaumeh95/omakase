fa56ace8e3a81ab34be86847392ad48a01d5c8cb
Guillaume Hercot <guillaume.hercot@gmail.com>
2017-12-14 17:43:10 +0100

    cards all good

--- app/javascript/components/cards_dashboard.js
+++ app/javascript/components/cards_dashboard.js
@@ -7,18 +7,27 @@ if (cards) {
       const target = event.currentTarget.querySelector(".trip-actions");
       target.classList.remove('hidden');
       target.classList.add('fade-card');
+      // Animate title
       const title = event.currentTarget.querySelector(".trip-title");
-      title.classList.remove('trip-title');
-      title.classList.add('trip-title-large', 'fade-card');
-
+      title.classList.add('animated', 'fadeOut');
+      // Animate actions menu
+      const tripActions = event.currentTarget.querySelector(".trip-actions-btn");
+      tripActions.classList.add('animated', 'fadeInDown');
+      // Animate date
+      const tripDate = event.currentTarget.querySelector(".trip-date-info");
+      tripDate.classList.add('animated', 'fadeInDown');
     });
     card.addEventListener("mouseleave", (event) => {
       const target = event.currentTarget.querySelector(".trip-actions");
       target.classList.add('hidden');
       target.classList.remove('fade-card');
-      const title = event.currentTarget.querySelector(".trip-title-large");
-      title.classList.remove('trip-title-large');
-      title.classList.add('trip-title', 'fade-card');
+      // Animate title
+      const title = event.currentTarget.querySelector(".trip-title");
+      title.classList.remove('animated', 'fadeOut');
+      title.classList.add('fade-card', 'animated', 'fadeIn');
     });
   });
 }
+
+
+
